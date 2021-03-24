eba184b88a65301bb285af15b9f86bd2f9282275
Leonardo Alves Prates <Leonardo.Prates@raizen.com>
2021-03-24 16:10:40 -0300

    adding provider file

---
+++ lab/terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
