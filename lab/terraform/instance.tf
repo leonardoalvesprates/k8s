6d04885bda0eebbe382d793409512bd29cd09fd4
Leonardo Alves Prates <Leonardo.Prates@raizen.com>
2021-03-25 17:08:29 -0300

    def public ip

--- lab/terraform/instance.tf
+++ lab/terraform/instance.tf
@@ -1,8 +1,9 @@
 
 resource "aws_instance" "vm_k8slab" {
-  ami           = "ami-05fc56020e2f7027a"
-  instance_type = "t3.medium"
-  key_name      = "leoaws"
+  ami                        = "ami-05fc56020e2f7027a"
+  instance_type              = "t3.medium"
+  key_name                   = "leoaws"
+  associate_public_ip_address = "true"
 
   network_interface {
     network_interface_id = aws_network_interface.if_k8slab.id
