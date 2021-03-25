b25254badf4786cf474f34862c3cd4a029b8b88b
Leonardo Alves Prates <Leonardo.Prates@raizen.com>
2021-03-25 18:06:39 -0300

    adding access and secret key for backend s3

--- lab/terraform/provider.tf
+++ lab/terraform/provider.tf
@@ -6,9 +6,11 @@ terraform {
     }
   }
   backend "s3" {
-  bucket = "labtfstate"
-  key    = "tfstate/labk8s.tfstate"
-  region = "us-east-1"
+  bucket     = "labtfstate"
+  key        = "tfstate/labk8s.tfstate"
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   }
 }
 
