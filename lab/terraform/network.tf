2e508c0c717d2a9ab6df4155097ac530cfc2409d
Leonardo Alves Prates <Leonardo.Prates@raizen.com>
2021-03-26 13:58:28 -0300

    commented out network resources

--- lab/terraform/network.tf
+++ lab/terraform/network.tf
@@ -1,22 +1,22 @@
-resource "aws_vpc" "vpc_lab" {
-  cidr_block = "10.0.0.0/16"
-}
+# resource "aws_vpc" "vpc_lab" {
+#   cidr_block = "10.0.0.0/16"
+# }
 
-resource "aws_subnet" "subnet_lab" {
-  vpc_id            = aws_vpc.vpc_lab.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+# resource "aws_subnet" "subnet_lab" {
+#   vpc_id            = aws_vpc.vpc_lab.id
+#   cidr_block        = "10.0.0.0/24"
+#   availability_zone = "us-east-1a"
 
-  tags = {
-    Name = "K8s LAB"
-  }
-}
+#   tags = {
+#     Name = "K8s LAB"
+#   }
+# }
 
-resource "aws_network_interface" "if_k8slab" {
-  subnet_id   = aws_subnet.subnet_lab.id
-#   private_ips = ["172.16.10.100"]
+# resource "aws_network_interface" "if_k8slab" {
+#   subnet_id   = aws_subnet.subnet_lab.id
+# #   private_ips = ["172.16.10.100"]
 
-  tags = {
-    Name = "K8s LAB"
-  }
-}
+#   tags = {
+#     Name = "K8s LAB"
+#   }
+# }
