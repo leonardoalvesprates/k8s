bdeeaa6450e20e6262381439ead731e552001cd7
Leonardo Alves Prates <Leonardo.Prates@raizen.com>
2021-03-26 16:12:58 -0300

    adding security group for instance

--- lab/terraform/instance.tf
+++ lab/terraform/instance.tf
@@ -1,9 +1,10 @@
 
 resource "aws_instance" "vm_k8slab" {
-  ami                        = "ami-05fc56020e2f7027a"
-  instance_type              = "t3.medium"
-  key_name                   = "leoaws"
+  ami                         = "ami-05fc56020e2f7027a"
+  instance_type               = "t3.medium"
+  key_name                    = "leoaws"
   associate_public_ip_address = "true"
+  security_groups             = "allow_all"
 
   # network_interface {
   #   network_interface_id = aws_network_interface.if_k8slab.id
@@ -18,4 +19,29 @@ resource "aws_instance" "vm_k8slab" {
     Name = "K8s LAB"
   }
 
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow ALL inbound traffic"
+
+  ingress {
+    description = "ALL from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "K8s LAB"
+  }
 }
