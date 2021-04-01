ffbf1db74aaabd6f3c85bb6d24ea9fc4e9cc2de0
Leonardo Alves Prates <Leonardo.Prates@raizen.com>
2021-04-01 13:24:47 -0300

    adding sleep 30s at provisionar remote-exec command

--- lab/terraform/instance.tf
+++ lab/terraform/instance.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "vm_k8slab" {
   }
 
   provisioner "remote-exec" {
-    inline = ["echo 'wait until SSH is ready'"]
+    inline = ["echo 'wait until SSH is ready'; sleep 30"]
 
     connection {
       type = "ssh"
