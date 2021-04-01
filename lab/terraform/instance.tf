ff9b025767501b6bb7f1f03f266cbda3366e6818
Leonardo Alves Prates <Leonardo.Prates@raizen.com>
2021-04-01 13:06:37 -0300

    changing instance local-exec command

--- lab/terraform/instance.tf
+++ lab/terraform/instance.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "vm_k8slab" {
   }
 
   provisioner "local-exec" {
-    command = "ansible-playbook -i ${aws_instance.vm_k8slab.public_ip}, --private-key ./file.pem k8s.yaml"
+    command = "ansible-playbook -i ${aws_instance.vm_k8slab.public_ip}, --private-key ./file.pem ../ansible/k8s.yaml"
   }
 
   tags = {
