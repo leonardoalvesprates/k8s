3df0bc80cc49660bcd912f0ac5a8c9e6e3dc2a59
Leonardo Alves Prates <leonardo.prates@compasso.com.br>
2021-06-19 11:18:11 -0300

    TF fmt

--- lab/single-node/terraform/variables.tf
+++ lab/single-node/terraform/variables.tf
@@ -1,3 +1,3 @@
-variable AWS_ACCESS_KEY_ID {}
-variable AWS_SECRET_ACCESS_KEY {}
-variable AWS_KEY_SSH {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_KEY_SSH" {}
