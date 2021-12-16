96fcab6eee4c80a19555ffe79b5f06accefde6b9
Leonardo Prates <leonardoalvesprates@gmail.com>
2021-12-16 19:30:28 -0300

    rancher2 TF tests

---
+++ lab/rancher/terraform/rancher2.tf
@@ -0,0 +1,7 @@
+resource "rancher2_cloud_credential" "aws" {
+  name = var.cloud_aws_cred
+  amazonec2_credential_config {
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
+  }
+}
--- lab/rancher/terraform/vars.tf
+++ lab/rancher/terraform/vars.tf
@@ -1,1 +1,6 @@
 variable rancher2_token_key {}
+variable cloud_aws_cred { 
+    default = aws
+}
+variable aws_access_key {}
+variable aws_secret_key {}
