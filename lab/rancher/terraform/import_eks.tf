e76e20a431936807a089630dea037eaaffd2da3b
Leonardo Prates <leonardoalvesprates@gmail.com>
2021-12-16 20:10:52 -0300

    tests

--- lab/rancher/terraform/import_eks.tf
+++ lab/rancher/terraform/import_eks.tf
@@ -2,12 +2,9 @@ resource rancher2_cluster eks_import_01 {
   name = "imported-eks-01"
   description = "lprates lab environment"
   eks_config_v2 {
-    cloud_credential_id = rancher2_cloud_credential.aws
+    cloud_credential_id = rancher2_cloud_credential.aws.id
     name = "eks-dec16-lab3"
     region = "us-east-1"
     imported = true
   }
-  depends_on = [
-    rancher2_cloud_credential.aws
-  ]
 }
