e3ca470563463a44b5dd81bc30255cb5bc9bf49e
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-03 12:22:33 -0300

    lab

--- lab/rancher/terraform/create_eks/create_eks.tf
+++ lab/rancher/terraform/create_eks/create_eks.tf
@@ -5,7 +5,7 @@ resource "rancher2_cluster" "eks_create_01" {
     cloud_credential_id = rancher2_cloud_credential.aws.id
     name = var.aws_eks_name
     region = var.aws_region
-    imported = no
+    imported = false
     node_groups {
       name = "ng01"
     }
