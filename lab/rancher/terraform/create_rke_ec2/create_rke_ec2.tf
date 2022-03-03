9341156d1c2513bba1c0fa2ca52568fc11060a90
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-03 20:47:00 -0300

    lab

--- lab/rancher/terraform/create_rke_ec2/create_rke_ec2.tf
+++ lab/rancher/terraform/create_rke_ec2/create_rke_ec2.tf
@@ -15,3 +15,35 @@ resource "rancher2_node_template" "rke_template" {
     #zone = var.aws_zone
   }
 }
+
+resource "rancher2_cluster" "rke_ec2" {
+  name = var.rke_cluster_name
+  description = "lprates lab"
+  rke_config {
+    network {
+      plugin = "canal"
+    }
+  }
+}
+
+resource "rancher2_node_pool" "cp26" {
+  cluster_id =  rancher2_cluster.rke_ec2.id
+  name = "cp26-"
+  hostname_prefix =  "cp26-"
+  node_template_id = rancher2_node_template.rke_template.id
+  quantity = 1
+  control_plane = true
+  etcd = true
+  worker = false
+}
+
+resource "rancher2_node_pool" "wk26" {
+  cluster_id =  rancher2_cluster.rke_ec2.id
+  name = "wk26-"
+  hostname_prefix = "wk26-"
+  node_template_id = rancher2_node_template.rke_template.id
+  quantity = 1
+  control_plane = false
+  etcd = false
+  worker = true
+}
