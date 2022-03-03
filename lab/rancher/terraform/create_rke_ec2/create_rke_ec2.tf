8211b6771458238f760298af9bcb1a0818468a5c
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-03 20:52:51 -0300

    lab

--- lab/rancher/terraform/create_rke_ec2/create_rke_ec2.tf
+++ lab/rancher/terraform/create_rke_ec2/create_rke_ec2.tf
@@ -28,8 +28,8 @@ resource "rancher2_cluster" "rke_ec2" {
 
 resource "rancher2_node_pool" "cp26" {
   cluster_id =  rancher2_cluster.rke_ec2.id
-  name = "cp26-"
-  hostname_prefix =  "cp26-"
+  name = "cp"
+  hostname_prefix = "cp"
   node_template_id = rancher2_node_template.rke_template.id
   quantity = 1
   control_plane = true
@@ -39,8 +39,8 @@ resource "rancher2_node_pool" "cp26" {
 
 resource "rancher2_node_pool" "wk26" {
   cluster_id =  rancher2_cluster.rke_ec2.id
-  name = "wk26-"
-  hostname_prefix = "wk26-"
+  name = "wk"
+  hostname_prefix = "wk"
   node_template_id = rancher2_node_template.rke_template.id
   quantity = 1
   control_plane = false
