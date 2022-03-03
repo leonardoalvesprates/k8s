e1f031de76f499281ba709bde30c6e4a79e7cf83
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-03 20:28:16 -0300

    lab

--- lab/rancher/terraform/create_rke_ec2/create_rke_ec2.tf
+++ lab/rancher/terraform/create_rke_ec2/create_rke_ec2.tf
@@ -5,14 +5,14 @@ resource "rancher2_node_template" "rke_template" {
     access_key = var.aws_access_key
     secret_key = var.aws_secret_key
     ami = var.aws_ami
-    #region = var.aws_region
+    region = var.aws_region
     security_group = [var.aws_nsg]
     subnet_id = var.aws_subnet
     vpc_id = var.aws_vpc
     zone = var.aws_region
     instance_type = var.aws_instance_type
     root_size = "30"
-    zone = var.aws_zone
+    #zone = var.aws_zone
   }
 }
 
