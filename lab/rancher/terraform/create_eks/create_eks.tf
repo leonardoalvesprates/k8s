476a61f1f7a9ae81afeae4f24d4785e8f26dc6b4
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-03 12:27:41 -0300

    lab

--- lab/rancher/terraform/create_eks/create_eks.tf
+++ lab/rancher/terraform/create_eks/create_eks.tf
@@ -6,6 +6,7 @@ resource "rancher2_cluster" "eks_create_01" {
     name = var.aws_eks_name
     region = var.aws_region
     imported = false
+    public_access = true
     node_groups {
       name = "ng01"
     }
