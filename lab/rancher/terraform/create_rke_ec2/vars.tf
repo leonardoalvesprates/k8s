d6d087d40955ffece1b2101680b8054f24ec3df4
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-03 22:10:29 -0300

    lab

--- lab/rancher/terraform/create_rke_ec2/vars.tf
+++ lab/rancher/terraform/create_rke_ec2/vars.tf
@@ -26,7 +26,7 @@ variable aws_nsg {
     default = "all-open"
 }
 variable aws_ami {
-    default = "ami-01b996646377b6619"
+    default = "ami-04cc2b0ad9e30a9c8"
 }
 variable aws_instance_type {
     default = "t3a.large"
