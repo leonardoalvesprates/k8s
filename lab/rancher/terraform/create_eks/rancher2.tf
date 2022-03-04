417f9099b62617fe29f85ed33ff6ad05b35f9179
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-04 13:28:18 -0300

    lab

--- lab/rancher/terraform/create_eks/rancher2.tf
+++ lab/rancher/terraform/create_eks/rancher2.tf
@@ -1,5 +1,5 @@
 resource "rancher2_cloud_credential" "aws" {
-  name = "aws"
+  name = "aws-eks"
   amazonec2_credential_config {
     access_key = var.aws_access_key
     secret_key = var.aws_secret_key
