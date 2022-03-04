d9c33b14fba39c0e4bc2e5b2b680701785dfd846
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-03 21:30:52 -0300

    lab

--- lab/rancher/terraform/create_rke_ec2/create_rke_ec2.tf
+++ lab/rancher/terraform/create_rke_ec2/create_rke_ec2.tf
@@ -13,7 +13,7 @@ resource "rancher2_node_template" "rke_template" {
     zone = var.aws_region
     instance_type = var.aws_instance_type
     root_size = "30"
-    zone = var.aws_zone
+    #zone = var.aws_zone
     iam_instance_profile = "rk-all-role"
     tags = [Owner,lprates]
   }
