ffd78fb6c9ee11d74e38f2fe391243c2c67cd69b
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-07 11:15:33 -0300

    lab

--- lab/rancher/terraform/create_eks/vars.tf
+++ lab/rancher/terraform/create_eks/vars.tf
@@ -1,13 +1,16 @@
 variable rancher2_token_key {}
 variable aws_access_key {}
 variable aws_secret_key {}
-# variable aws_kms_key {}
+variable aws_kms_key {}
+
 variable rancher_url {
     default = "https://rancher26.prateslabs.com.br"
 }
+
 variable aws_eks_name {
     default = "lprates-eks-mar22-7"
 }
+
 variable aws_region {
     default = "sa-east-1"
 }
