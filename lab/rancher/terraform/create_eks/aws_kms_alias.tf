331361d1e26af0bfbab15ff16bc54e6a1fb36467
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-07 10:04:07 -0300

    lab

--- lab/rancher/terraform/create_eks/aws_kms_alias.tf
+++ lab/rancher/terraform/create_eks/aws_kms_alias.tf
@@ -4,6 +4,6 @@ resource "aws_kms_key" "kms01" {
 }
 
 resource "aws_kms_alias" "kms_alias01" {
-  name          = "lprates-lab2"
+  name          = "alias/lprates-lab2"
   target_key_id = aws_kms_key.kms01.key_id
 }
