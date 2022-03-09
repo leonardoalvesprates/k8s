ed30eb50a77e03333430d7927b69c18b58354ad4
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-09 11:56:30 -0300

    lab

--- lab/rancher/terraform/create_eks/create_eks.tf
+++ lab/rancher/terraform/create_eks/create_eks.tf
@@ -8,11 +8,11 @@ resource "rancher2_cluster" "eks_create_01" {
     imported = false
     public_access = true
     kubernetes_version = "1.21"
-    kms_key = "f278a40e-be26-431c-b939-9608749ed85e"
+    # kms_key = "f278a40e-be26-431c-b939-9608749ed85e"
     # kms_key = "alias/lprates-lab-mar-9-t3"
     # kms_key = aws_kms_key.kms01.key_id
     # kms_key = aws_kms_alias.kms_alias01.name
-    secrets_encryption = true
+    # secrets_encryption = true
     node_groups {
       name = "ng01"
     }
