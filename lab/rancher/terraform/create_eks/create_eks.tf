a2b12dbcc2343684fd50a352c72a4c77a8a179ff
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-03-09 14:51:19 -0300

    lab

--- lab/rancher/terraform/create_eks/create_eks.tf
+++ lab/rancher/terraform/create_eks/create_eks.tf
@@ -12,7 +12,7 @@ resource "rancher2_cluster" "eks_create_01" {
     # kms_key = "alias/lprates-lab-mar-9-t3"
     # kms_key = aws_kms_key.kms01.key_id
     # kms_key = aws_kms_alias.kms_alias01.name
-    # secrets_encryption = true
+    secrets_encryption = true
     node_groups {
       name = "ng01"
     }
