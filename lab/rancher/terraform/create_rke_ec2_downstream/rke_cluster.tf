f3e9f589ff368989cbe38bf62fd6985ce64ca14d
Leonardo Prates <leonardoalvesprates@gmail.com>
2022-05-04 16:06:03 -0300

    lab

--- lab/rancher/terraform/create_rke_ec2_downstream/rke_cluster.tf
+++ lab/rancher/terraform/create_rke_ec2_downstream/rke_cluster.tf
@@ -2,6 +2,7 @@ resource "rancher2_cluster" "rke_ec2" {
   name = var.rke_cluster_name
   description = "lprates lab"
   rke_config {
+    kubernetes_version = "v1.20.15-rancher1-3"
     network {
       plugin = "canal"
     }
