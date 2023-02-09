7290fdad47ad840d60516263c653dfde8f5edb4e
leonardoalvesprates <leonardoalvesprates@gmail.com>
2023-02-09 13:42:16 -0300

    lab

--- lab/rancher/workaround/remove_tf_tokens/delete_user_tf_tokens_before_date.bash
+++ lab/rancher/workaround/remove_tf_tokens/delete_user_tf_tokens_before_date.bash
@@ -5,3 +5,6 @@ for USERTOKEN in $(echo $TOKENS_BY_TF_USER)
 do 
   kubectl get tokens.management.cattle.io $USERTOKEN -o json | jq 'select (.metadata.creationTimestamp<"2023-02-10") | .metadata.name ' | sed 's/"//g' | xargs kubectl delete tokens.management.cattle.io
 done
+
+# error: resource(s) were provided, but no name was specified
+# This is expeded if the token doesn't match the defined creationTimestamp
