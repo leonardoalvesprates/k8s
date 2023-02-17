86b26675a1a73359483addfc13e7e9ec2a2c14eb
leonardoalvesprates <leonardoalvesprates@gmail.com>
2023-02-17 16:05:23 -0300

    rke2 tf lab

---
+++ lab/rancher/terraform/create_rke2_ec2_downstream/main.tf
@@ -0,0 +1,41 @@
+resource "rancher2_machine_config_v2" "machine" {
+  generate_name = "${var.prefix}-${random_string.random.result}"
+  amazonec2_config {
+    ami            = data.aws_ami.ubuntu.id
+    region         = var.aws_region
+    instance_type  = var.aws_instance_type
+    security_group = [var.aws_nsg]
+    subnet_id      = var.aws_subnet
+    vpc_id         = var.aws_vpc
+    zone           = var.aws_zone
+    iam_instance_profile = var.iam_profile
+  }
+}
+
+resource "rancher2_cluster_v2" "rke2_cluster" {
+  name                                     = "${var.prefix}-${random_string.random.result}"
+  kubernetes_version                       = var.kubernetes_version  
+  enable_network_policy                    = false
+  depends_on = [
+    rancher2_cloud_credential.aws
+  ]
+  rke_config {
+    machine_pools {
+      name                         = "pool1"
+      cloud_credential_secret_name = rancher2_cloud_credential.aws.id
+      control_plane_role           = true
+      etcd_role                    = true
+      worker_role                  = true
+      quantity                     = 1
+      machine_config {
+        kind = rancher2_machine_config_v2.machine.kind
+        name = rancher2_machine_config_v2.machine.name
+      }
+    }
+    machine_global_config = <<EOF
+---
+cni: cilium
+EOF
+  }
+}
+
