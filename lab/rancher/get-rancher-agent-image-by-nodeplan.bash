5f74d8e34a95569fac8146e990bbe6c984fdd649
Leonardo Prates <leonardoalvesprates@gmail.com>
2024-08-23 14:19:04 -0300

    replacing echo with printf

--- lab/rancher/get-rancher-agent-image-by-nodeplan.bash
+++ lab/rancher/get-rancher-agent-image-by-nodeplan.bash
@@ -4,7 +4,7 @@ blue=$(tput setaf 6)
 yellow=$(tput setaf 220)
 normal=$(tput sgr0)
 
-printf "${yellow} ==== This works to gather RKE node info ONLY ==== \n"
+printf "${yellow}==== This works to gather RKE node info ONLY ==== \n"
 
 for CLUSTER in $(kubectl get clusters.management.cattle.io --no-headers -o custom-columns=":metadata.name")
 do
@@ -14,13 +14,11 @@ do
     printf "${normal}"
     
   else
-    printf "${yellow}"
-    echo $CLUSTER
+    printf "${yellow} ${CLUSTER}\n"
     for NODE in $(kubectl -n $CLUSTER get nodes.management.cattle.io --no-headers -o custom-columns=":metadata.name")
     do
       NODEIMAGE=$(kubectl -n $CLUSTER get nodes.management.cattle.io $NODE -o jsonpath='{.status.nodePlan.plan.processes.share-mnt.image}{"\n"}')
-      printf "${blue}"
-      echo $NODE $NODEIMAGE
+      printf "${blue} ${NODE} ${NODEIMAGE}\n"
       printf "${normal}"
     done
     printf "${normal} \n"
